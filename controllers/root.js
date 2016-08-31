69dbfc76d5ead0792259ed4ac58253c095da2cb7
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-08-31 15:24:30 +0200

    Handle request in multiple steps (chain)

--- controllers/root.js
+++ controllers/root.js
@@ -5,7 +5,7 @@ var log = require('./../lib/logger').logger.getLogger("Root");
 
 var Root = (function() {
 
-    var handleproxy = function(req, res, options, body){
+    var callPoxy = function(req, res, options, body){
 
       // Adds x-forwarded-for header
       options.headers = httpClient.getClientIp(req, req.headers);
@@ -21,20 +21,46 @@ var Root = (function() {
       httpClient.sendData(options, body, res);
     }
 
+    // Is experiment allowed to feed data
+    var call1 = function(req, res, options, body) {
+      call2(req, res, options, body);
+    };
+
+    // Is Experimenter of Experiment
+    var call2 = function(req, res, options, body) {
+      call3(req, res, options, body);
+    };
+
+    // Is Application of Experiment
+    var call3 = function(req, res, options, body) {
+      call4(req, res, options, body);
+    };
+
+    // Does the experiment have quota
+    var call4 = function(req, res, options, body) {
+      call5(req, res, options, body);
+    };
+
+    // Parse body
+    var call5 = function(req, res, options, body) {
+      callPoxy(req, res, options, body);
+    };
+
     var pep = function(req, res) {
 
-        var options = {
-            protocol: 'http',
-            host: config.app_host,
-            port: config.app_port,
-            path: req.url,
-            method: req.method,
-            headers: req.headers
-        };
+      var options = {
+          protocol: 'http',
+          host: config.app_host,
+          port: config.app_port,
+          path: req.url,
+          method: req.method,
+          headers: req.headers
+      };
 
-        var body = req.body.toString('utf8');
+      var body = req.body.toString('utf8');
 
-        handleproxy(req, res, options, body);
+      call1(req, res, options, body);
+
     };
 
     return {

ac472daba2a346e0510e4eb2ffbf0b65e6043075
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-09-13 17:11:00 +0200

    New config template

---
+++ config.template.js
@@ -0,0 +1,46 @@
+var config = {};
+
+config.port = 8000;
+
+// Set this var to undefined if you don't want the server to listen on HTTPS
+config.https = {
+    enabled: false,
+    cert_file: 'cert/cert.crt',
+    key_file: 'cert/key.key',
+    port: 443
+};
+
+config.client_id = "";
+config.client_secret = "";
+config.bad_asset_attributes = ['urn:oc:attributeType:reputation'];
+
+config.application_endpoint = {
+  protocol: 'http',
+	host: 'HOST',
+	port: 'PORT'
+};
+
+config.accounts_token_endpoint = {
+	protocol: 'https',
+	host: 'accounts.organicity.eu',
+	port: '443',
+	path: '/realms/organicity/protocol/openid-connect/token',
+};
+
+config.platform_management_api = {
+	protocol: 'http',
+	host: 'dev.server.organicity.eu',
+	port: '8080'
+};
+
+config.experiment_management_api = {
+	protocol: 'http',
+	host: 'pro.server.organicity.eu',
+	port: '8081',
+}
+
+// list of paths that will not check authentication/authorization
+// example: ['/public/*', '/static/css/']
+config.public_paths = [];
+
+module.exports = config;
